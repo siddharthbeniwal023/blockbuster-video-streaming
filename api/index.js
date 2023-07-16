3c8f0ece38903585c3f3d55b27dc645d3b4c2f9f
siddharthbeniwal023 <siddharthbeniwal2001@gmail.com>
2023-07-16 14:49:45 +0530

    REST Api implemented

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRouter = require("./routes/auth")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
@@ -14,7 +17,10 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
-app.use("/api/auth",authRouter);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(8800, () => {
   console.log("Backend Server in running");
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
-// Register
+//REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -15,32 +15,32 @@ router.post("/register", async (req, res) => {
   });
   try {
     const user = await newUser.save();
-    res.status(200).json(user);
+    res.status(201).json(user);
   } catch (err) {
-    res.status(500).json("Server Heavy");
+    res.status(500).json(err);
   }
 });
 
-//Login
+//LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(401).json("Wrong username or Password");
+    !user && res.status(401).json("Wrong password or username!");
 
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-    if (originalPassword !== req.body.password) {
-      res.status(401).json("Wrong username or Password");
-    }
-
-    const { password, ...info } = user._doc;
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username!");
 
     const accessToken = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
       process.env.SECRET_KEY,
       { expiresIn: "5d" }
     );
+
+    const { password, ...info } = user._doc;
+
     res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
